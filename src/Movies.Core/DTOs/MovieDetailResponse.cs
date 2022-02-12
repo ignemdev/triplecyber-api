73576252b3a8efad8d1b4e1ad13ee78a575873af
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-12 18:48:54 -0400

    dtos creation

---
+++ src/Movies.Core/DTOs/MovieDetailResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies.Core.DTOs
+{
+    public class MovieDetailResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public string PosterPath { get; set; }
+        public string BackdropPath { get; set; }
+        public double VoteAverage { get; set; }
+    }
+}
---
+++ src/Movies.Core/DTOs/MovieResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies.Core.DTOs
+{
+    public class MovieResponse <T> where T : class
+    {
+        public int Page { get; set; }
+        public IEnumerable<T> Results { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+}
