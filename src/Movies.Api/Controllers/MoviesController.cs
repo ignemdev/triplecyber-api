5f0d7170f6bbfdab7c9156b10dabde23233099e3
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-12 17:55:02 -0400

    movies endpoints created

---
+++ src/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        public MoviesController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetMoviesByPage([FromQuery] int page = 1)
+        {
+            return Ok(page);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetMovieById(int id)
+        {
+            return Ok(id);
+        }
+
+    }
+}
