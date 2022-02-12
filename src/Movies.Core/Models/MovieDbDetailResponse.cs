e98215ffdcc97b9484971cb3c6599df4abfaed38
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-12 18:39:17 -0400

    movie db response models

---
+++ src/Movies.Core/Models/MovieDbDetailResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies.Core.Models
+{
+    public class MovieDbDetailResponse
+    {
+        public bool Adult { get; set; }
+        public string BackdropPath { get; set; }
+        public int[] GenreIds { get; set; }
+        public int Id { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public double Popularity { get; set; }
+        public string PosterPath { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Title { get; set; }
+        public bool Video { get; set; }
+        public double VoteAverage { get; set; }
+        public double VoteCount { get; set; }
+    }
+}
---
+++ src/Movies.Core/Models/MovieDbResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Movies.Core.Models
+{
+    public class MovieDbResponse<T> where T : class
+    {
+        public int Page { get; set; }
+        public IEnumerable<T> Results { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+}
