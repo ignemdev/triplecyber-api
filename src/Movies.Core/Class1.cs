6285dcc3de8b91b6b67c4aa58876ca78984d2e75
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-11 23:32:18 -0400

    project structure created

---
+++ src/Movies.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Movies.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Movies.Utilities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Movies.Utilities
+{
+    public class Class1
+    {
+    }
+}
