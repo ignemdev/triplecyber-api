35c3cc6a07f516d941e8420da672029e3961f36c
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-12 18:49:08 -0400

    service contract created

---
+++ src/Movies.Core/Services/IMoviesServices.cs
@@ -0,0 +1,15 @@
+﻿using Movies.Core.DTOs;
+using Movies.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Core.Services
+{
+    public interface IMoviesServices
+    {
+        Task<MovieResponse<MovieDetailResponse>> GetMoviesByPageAsync(int page);
+        Task<MovieDetailResponse> GetMovieByIdAsync(int id);
+    }
+}
